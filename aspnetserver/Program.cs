3542003196be6c5234cf7b18bfe3c47670e53899
Volodimir <volodimirnazarisin@gmail.com>
2023-01-06 10:29:40 +0200

    Small changes to program.cs, no Actions should run.

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://gray-island-0c4561503.2.azurestaticapps.net");
         });
 });
 
